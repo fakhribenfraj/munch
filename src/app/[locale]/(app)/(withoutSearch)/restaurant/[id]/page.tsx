acb3ba35cfb2b7ecc59fdc585977982fcc42b8e2
fakhribenfraj <benfrajfakhreddine15@gmail.com>
2024-10-06 14:21:06 +0100

    made tabs sticky

--- src/app/[locale]/(app)/(withoutSearch)/restaurant/[id]/page.tsx
+++ src/app/[locale]/(app)/(withoutSearch)/restaurant/[id]/page.tsx
@@ -1,13 +1,14 @@
 import { getRestaurantById } from "@/actions/restaurants/getRestaurantById";
 import { getRestaurants } from "@/actions/restaurants/getRestaurants";
 import Carousel from "@/components/common/Carousel";
+import HideOnScroll from "@/components/common/navigation/HideOnScroll";
 import { Box, Button, Stack, Tab, Tabs, Typography } from "@mui/material";
 import Image from "next/image";
 
 export default async function Page({ params }: { params: { id: string } }) {
   const { data: restaurant } = await getRestaurantById(params.id);
   return (
-    <Stack gap={2}>
+    <Stack>
       <Stack alignItems="flex-start" gap={1}>
         <Box
           sx={{
@@ -37,22 +38,24 @@ export default async function Page({ params }: { params: { id: string } }) {
         <Typography variant="h4">{restaurant?.name}</Typography>
         <Button variant="outlined">add to favorite</Button>
       </Stack>
-      <Tabs
-        value="Overview"
-        aria-label="basic tabs example"
-        indicatorColor="primary"
-        textColor="primary"
-      >
-        {["Overview", "Menu", "Reviews", "Contact"].map((tab) => (
-          <Tab label={tab} key={tab} value={tab} />
-        ))}
-      </Tabs>
-      <Stack alignItems="flex-start" gap={1}>
-        <Typography variant="h6">description:</Typography>
-        <Typography variant="body1">{restaurant?.description}</Typography>
-      </Stack>
-      <Stack alignItems="flex-start" gap={1}>
-        <Typography variant="h6">media:</Typography>
+      <Stack gap={2}>
+        <Tabs
+          value="Overview"
+          aria-label="basic tabs example"
+          indicatorColor="primary"
+          textColor="primary"
+          sx={{
+            position: "sticky",
+            pt: 2,
+            top: 0,
+            backgroundColor: "grey.200",
+            zIndex: 1,
+          }}
+        >
+          {["Overview", "Menu", "Reviews", "Contact"].map((tab) => (
+            <Tab label={tab} key={tab} value={tab} />
+          ))}
+        </Tabs>
         <Carousel height={280} width={400}>
           {restaurant?.images.map((image, j) => (
             <Box key={`${restaurant.id}-${j}`}>
@@ -60,6 +63,20 @@ export default async function Page({ params }: { params: { id: string } }) {
             </Box>
           ))}
         </Carousel>
+        {[
+          "description",
+          "delegation",
+          "address",
+          "phone",
+          "email",
+          "website",
+        ].map((item) => (
+          <Stack alignItems="flex-start" gap={1}>
+            <Typography variant="h6">{item}:</Typography>
+            {/* @ts-ignore */}
+            <Typography variant="body1">{restaurant[item]}</Typography>
+          </Stack>
+        ))}
       </Stack>
     </Stack>
   );
