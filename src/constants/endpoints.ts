c1a59cea5684038f7b27e1bd06c9e234a7910e9e
fakhribenfraj <benfrajfakhreddine15@gmail.com>
2024-10-07 12:27:45 +0100

    changed mock url root

--- src/constants/endpoints.ts
+++ src/constants/endpoints.ts
@@ -1,9 +1,9 @@
 const BASE_URL = process.env.PORTAL_API_URL;
-const MOCK_URL = "https://localhost:3000/api ";
+const MOCK_URL = process.env.MOCK_PORTAL_API_URL;
 const endpoints = {
-  SIGNIN: `${MOCK_URL}/mock/signin`,
-  REGISTER: `${MOCK_URL}/mock/register`,
-  PROFILE: `${MOCK_URL}/mock/profile`,
+  SIGNIN: `${MOCK_URL}/signin`,
+  REGISTER: `${MOCK_URL}/register`,
+  PROFILE: `${MOCK_URL}/profile`,
   RESTAURANTS: `${BASE_URL}/restaurants`,
 };
 export default endpoints;
