99b51868ab6602ce83d42d52f6c9ec9e65ffdbb9
fakhribenfraj <benfrajfakhreddine15@gmail.com>
2024-10-21 11:15:24 +0100

    removed mock apis

--- src/app/api/mock/profile/route.ts
+++
@@ -1,9 +0,0 @@
-export async function GET(req: Request) {
-  return Response.json({
-    email: "benfrajfakhreddine15@gmail.com",
-    id: "1eeffdaghK2",
-    name: "fakhreddine",
-    birthdate: "15/06/1997",
-    country: "tunisia",
-  });
-}
