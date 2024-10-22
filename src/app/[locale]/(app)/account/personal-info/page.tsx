8f1cc43f4b49f0699d1a8d49732b0ca8155a6e94
fakhribenfraj <benfrajfakhreddine15@gmail.com>
2024-10-22 20:05:23 +0100

    added file input

--- src/app/[locale]/(app)/account/personal-info/page.tsx
+++ src/app/[locale]/(app)/account/personal-info/page.tsx
@@ -1,7 +1,8 @@
 import getProfile from "@/actions/profile/getProfile";
-import LogoutButton from "@/components/common/auth/LogoutButton";
+import FileInput from "@/components/common/inputs/FileInput";
 import ReturnButton from "@/components/common/navigation/ReturnButton";
-import { Box, List, ListItem, Typography } from "@mui/material";
+import PhotoCameraIcon from "@mui/icons-material/PhotoCamera";
+import { Avatar, Box, Chip, List, ListItem, Paper } from "@mui/material";
 import { NextPage } from "next";
 const Page: NextPage = async () => {
   const profile = await getProfile();
@@ -10,10 +11,44 @@ const Page: NextPage = async () => {
       <ReturnButton
         label="profile"
         sx={{
-          mb: 6,
+          mb: 2,
         }}
       />
       <List>
+        <ListItem>
+          <Box sx={{ position: "relative", margin: "auto",mb:2 }}>
+            <Avatar
+              sx={{
+                width: 160,
+                height: 160,
+                bgcolor: "grey.400",
+              }}
+              alt={profile.data.name}
+              src={profile.data.avatar}
+            />
+            <FileInput
+              id="avatar"
+              accept="image/*"
+              sx={{
+                position: "absolute",
+                bottom: 0,
+                left: "50%",
+                transform: "translate(-50%,50%)",
+              }}
+            >
+              <Chip
+                icon={<PhotoCameraIcon />}
+                label="Edit"
+                variant="filled"
+                color="warning"
+                sx={{
+                  bgcolor: "common.white",
+                  boxShadow: 4,
+                }}
+              />
+            </FileInput>
+          </Box>
+        </ListItem>
         {Object.entries(profile.data).map(([key, value]) => (
           <ListItem key={key}>{`${key}: ${value}`}</ListItem>
         ))}
--- src/components/common/inputs/FileInput.tsx
+++ src/components/common/inputs/FileInput.tsx
@@ -1,28 +1,14 @@
 "use client";
-import {
-  Box,
-  Button,
-  ButtonGroup,
-  IconButton,
-  List,
-  ListItem,
-  Paper,
-  Stack,
-  Typography,
-} from "@mui/material";
-import { useEffect, useState } from "react";
-import CloudUploadIcon from "@mui/icons-material/CloudUpload";
-import AttachFileIcon from "@mui/icons-material/AttachFile";
-import DeleteIcon from "@mui/icons-material/Delete";
-import ClearIcon from "@mui/icons-material/Clear";
-type FileInputProps = {
-  id: string;
+import { Box, BoxProps, Stack } from "@mui/material";
+import { ReactNode, useEffect, useState } from "react";
+type FileInputProps = BoxProps & {
+  id: string; //used for linking label and input
   onChange?: (files: File[]) => void;
-  accept: string;
+  accept?: string;
+  children: ReactNode;
 };
-const FileInput = ({ accept, id, onChange }: FileInputProps) => {
+const FileInput = ({ children, accept, id, onChange, sx }: FileInputProps) => {
   const [files, setFiles] = useState<File[]>([]);
-  const [fileEnter, setFileEnter] = useState(false);
   useEffect(() => {
     if (files) {
       onChange && onChange(files);
@@ -30,131 +16,28 @@ const FileInput = ({ accept, id, onChange }: FileInputProps) => {
   }, [files, onChange]);
 
   return (
-    <Stack rowGap={2}>
-      <Paper
-        sx={{
-          display: "flex",
-          border: 0.5,
-          borderColor: "grey.400",
-          color: "grey.500",
-          backgroundColor: "grey.100",
-          alignItems: "center",
-          overflow: "hidden",
-          alignSelf: "flex-end",
+    <Box
+      sx={{
+        cursor: "pointer",
+        ...sx,
+      }}
+      component="label"
+      htmlFor={id}
+    >
+      {children}
+      <input
+        id={id}
+        type="file"
+        hidden
+        accept={accept}
+        onChange={(e) => {
+          let files = e.target.files;
+          if (files && files?.length > 0) {
+            setFiles(Array.from(files));
+          }
         }}
-        component="label"
-        htmlFor={id}
-      >
-        {files.length > 0 && (
-          <IconButton
-            sx={{ ml: "auto" }}
-            onClick={() => {
-              setFiles([]);
-            }}
-          >
-            <ClearIcon />
-          </IconButton>
-        )}
-        <Typography sx={{ px: 1 }}>
-          {files.length > 0 ? "Clear files" : "Choose files to upload"}
-        </Typography>
-        <Typography
-          sx={{
-            backgroundColor: "primary.main",
-            color: "common.white",
-            p: 1,
-            cursor: "pointer",
-          }}
-        >
-          Browse files
-        </Typography>
-      </Paper>
-      {files.length == 0 ? (
-        <Paper
-          sx={{
-            border: fileEnter ? 4 : 2,
-            borderStyle: "dashed",
-            borderColor: "grey.300",
-            color: "grey.500",
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-            p: { xs: 2, md: 4 },
-            backgroundColor: "grey.100",
-          }}
-          onDragOver={(e) => {
-            e.preventDefault();
-            setFileEnter(true);
-          }}
-          onDragLeave={(e) => {
-            setFileEnter(false);
-          }}
-          onDragEnd={(e) => {
-            e.preventDefault();
-            setFileEnter(false);
-          }}
-          onDrop={(e) => {
-            e.preventDefault();
-            setFileEnter(false);
-            if (e.dataTransfer.items) {
-              Array.from(e.dataTransfer.items).forEach((item, i) => {
-                if (item.kind === "file") {
-                  const file = item.getAsFile();
-                  if (file) {
-                    setFiles((files) => [...files, file]);
-                  }
-                }
-              });
-            } else {
-              Array.from(e.dataTransfer.files).forEach((file, i) => {
-                console.log(`… file[${i}].name = ${file.name}`);
-              });
-            }
-          }}
-        >
-          <Stack alignItems="center" rowGap={1}>
-            <CloudUploadIcon fontSize="large" />
-            <Typography>Drag and drop files here</Typography>
-          </Stack>
-          <input
-            id={id}
-            type="file"
-            hidden
-            accept={accept}
-            onChange={(e) => {
-              let files = e.target.files;
-              if (files && files?.length > 0) {
-                setFiles(Array.from(files));
-              }
-            }}
-          />
-        </Paper>
-      ) : (
-        <List>
-          {files.map((file) => (
-            <ListItem
-              key={file.name}
-              sx={{
-                boxShadow: 2,
-              }}
-            >
-              <AttachFileIcon />
-              <Typography sx={{ mx: 2 }}>{file.name}</Typography>
-              <IconButton
-                sx={{ ml: "auto" }}
-                onClick={() => {
-                  setFiles((files) =>
-                    files.filter((f) => f.name !== file.name)
-                  );
-                }}
-              >
-                <DeleteIcon />
-              </IconButton>
-            </ListItem>
-          ))}
-        </List>
-      )}
-    </Stack>
+      />
+    </Box>
   );
 };
 export default FileInput;
