bd4e6bdc44674eaaf48e461f16eda5288c7bf746
fakhribenfraj <benfrajfakhreddine15@gmail.com>
2024-10-31 21:07:07 +0100

    updrade mui

--- src/app/[locale]/(app)/account/page.tsx
+++ src/app/[locale]/(app)/account/page.tsx
@@ -6,6 +6,7 @@ import SettingsOutlinedIcon from "@mui/icons-material/SettingsOutlined";
 import SecurityIcon from "@mui/icons-material/Security";
 import {
   Box,
+  Grid2,
   List,
   ListItem,
   ListItemButton,
@@ -18,40 +19,48 @@ import { NextPage } from "next";
 const Home: NextPage = () => {
   return (
     <Stack>
-      <Typography variant="h2">Profile</Typography>
+      <Typography variant="h2">Account</Typography>
       <List>
-        {[
-          {
-            label: "personal info",
-            url: "/personal-info",
-            icon: <AccountCircleOutlinedIcon />,
-          },
-          {
-            label: "Privacy & security",
-            url: "/privacy",
-            icon: <SecurityIcon />,
-          },
-          {
-            label: "Settings",
-            url: "/settings",
-            icon: <SettingsOutlinedIcon />,
-          },
-        ].map((item) => (
-          <ListItem key={`profile-${item.label}`}>
-            <ListItemButton
-              href={`${routes.ACCOUNT}/${item.url}`}
-              sx={{ justifyContent: "space-between" }}
-            >
-              <Box sx={{ display: "flex" }}>
-                <ListItemIcon>{item.icon}</ListItemIcon>
-                <ListItemText>{item.label}</ListItemText>
-              </Box>
-              <ListItemIcon>
-                <NavigateNextOutlinedIcon />
-              </ListItemIcon>
-            </ListItemButton>
-          </ListItem>
-        ))}
+        <Grid2 container>
+          {[
+            {
+              label: "personal info",
+              url: "/personal-info",
+              icon: <AccountCircleOutlinedIcon />,
+            },
+            {
+              label: "Privacy & security",
+              url: "/privacy",
+              icon: <SecurityIcon />,
+            },
+            {
+              label: "Settings",
+              url: "/settings",
+              icon: <SettingsOutlinedIcon />,
+            },
+          ].map((item) => (
+            <Grid2 key={`profile-${item.label}`} size={{ xs: 12, md: 4 }}>
+              <ListItem>
+                <ListItemButton
+                  href={`${routes.ACCOUNT}/${item.url}`}
+                  sx={{ justifyContent: "space-between" }}
+                >
+                  <Box sx={{ display: "flex" }}>
+                    <ListItemIcon>{item.icon}</ListItemIcon>
+                    <ListItemText>{item.label}</ListItemText>
+                  </Box>
+                  <ListItemIcon
+                    sx={{
+                      display: { md: "none" },
+                    }}
+                  >
+                    <NavigateNextOutlinedIcon />
+                  </ListItemIcon>
+                </ListItemButton>
+              </ListItem>
+            </Grid2>
+          ))}
+        </Grid2>
       </List>
       <LogoutButton sx={{ display: { md: "none" } }} />
     </Stack>
