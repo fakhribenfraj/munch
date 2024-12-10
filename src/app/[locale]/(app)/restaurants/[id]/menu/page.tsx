29a0a55df8ae00a4602d953f8396932ccce68078
fakhribenfraj <benfrajfakhreddine15@gmail.com>
2024-12-10 12:51:39 +0100

    updated menu page

--- src/app/[locale]/(app)/restaurants/[id]/menu/page.tsx
+++ src/app/[locale]/(app)/restaurants/[id]/menu/page.tsx
@@ -1,11 +1,18 @@
 import { getRestaurantById } from "@/actions/restaurants/getRestaurantById";
-import MealCard from "@/components/custom/restaurant/MealCard";
 import RestaurantNavTabs from "@/components/custom/restaurant/RestaurantNavTabs";
 import Searchbar from "@/components/filters/Searchbar";
+import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
+import StarIcon from "@mui/icons-material/Star";
 import {
-  Grid2,
+  Box,
+  Card,
+  CardContent,
+  CardMedia,
+  Grid,
+  IconButton,
   Stack,
-  Toolbar
+  Toolbar,
+  Typography,
 } from "@mui/material";
 
 export default async function Page({
@@ -16,6 +23,76 @@ export default async function Page({
   const { id } = await params;
 
   const { data: restaurant } = await getRestaurantById(id);
+  const categories = [
+    {
+      title: "Burger",
+      items: [
+        {
+          name: "Chicken Burger",
+          price: "6.00 DT",
+          rating: 4.9,
+          image: "https://images.unsplash.com/photo-1551963831-b3b1ca40c98e",
+        },
+        {
+          name: "Beef Burger",
+          price: "10.00 DT",
+          rating: 4.9,
+          image: "https://images.unsplash.com/photo-1551782450-a2132b4ba21d",
+        },
+      ],
+    },
+    {
+      title: "Noodles",
+      items: [
+        {
+          name: "Ramen Noodles",
+          price: "15.00 DT",
+          rating: 4.9,
+          image: "https://images.unsplash.com/photo-1551782450-a2132b4ba21d",
+        },
+        {
+          name: "Pho Noodles",
+          price: "20.00 DT",
+          rating: 4.9,
+          image: "https://images.unsplash.com/photo-1551782450-a2132b4ba21d",
+        },
+      ],
+    },
+    {
+      title: "Spaghettis",
+      items: [
+        {
+          name: "Spaghetti Penne",
+          price: "12.00 DT",
+          rating: 4.9,
+          image: "https://images.unsplash.com/photo-1551782450-a2132b4ba21d",
+        },
+        {
+          name: "Spaghetti Farfalle",
+          price: "25.00 DT",
+          rating: 4.9,
+          image: "https://images.unsplash.com/photo-1551782450-a2132b4ba21d",
+        },
+      ],
+    },
+    {
+      title: "5maj",
+      items: [
+        {
+          name: "Fresh Fruit Donuts",
+          price: "5.00 DT",
+          rating: 4.9,
+          image: "https://images.unsplash.com/photo-1551782450-a2132b4ba21d",
+        },
+        {
+          name: "Rotini",
+          price: "18.00 DT",
+          rating: 4.9,
+          image: "https://images.unsplash.com/photo-1551782450-a2132b4ba21d",
+        },
+      ],
+    },
+  ];
   const itemData = [
     {
       img: "https://images.unsplash.com/photo-1551963831-b3b1ca40c98e",
@@ -115,13 +192,77 @@ export default async function Page({
       >
         <Searchbar />
       </Toolbar>
-      <Grid2 container spacing={2} mt={2}>
+      {/* <Grid2 container spacing={2} mt={2}>
         {itemData.map((item, index) => (
           <Grid2 size={{ xs: 6, sm: 4, md: 3, lg: 2 }} key={item.title + index}>
             <MealCard {...item} />
           </Grid2>
         ))}
-      </Grid2>
+      </Grid2> */}
+      <Box sx={{ padding: 2 }}>
+        {categories.map((category, index) => (
+          <Box key={index} mb={4}>
+            {/* Category Title */}
+            <Typography variant="h6" fontWeight="bold" mb={2}>
+              {category.title}
+            </Typography>
+
+            {/* Food Items Grid */}
+            <Grid container spacing={2}>
+              {category.items.map((item, idx) => (
+                <Grid item xs={6} sm={4} md={3} key={idx}>
+                  <Card sx={{ position: "relative" }}>
+                    {/* Food Image */}
+                    <CardMedia
+                      component="img"
+                      height="140"
+                      image={item.image}
+                      alt={item.name}
+                    />
+                    {/* Favorite Button */}
+                    <IconButton
+                      sx={{
+                        position: "absolute",
+                        top: 8,
+                        right: 8,
+                        backgroundColor: "white",
+                        zIndex: 1,
+                      }}
+                    >
+                      <FavoriteBorderIcon fontSize="small" />
+                    </IconButton>
+
+                    {/* Food Info */}
+                    <CardContent>
+                      <Typography variant="subtitle1" fontWeight="bold" noWrap>
+                        {item.name}
+                      </Typography>
+                      <Box display="flex" alignItems="center" mt={1}>
+                        <StarIcon fontSize="small" color="warning" />
+                        <Typography
+                          variant="body2"
+                          color="text.secondary"
+                          ml={0.5}
+                        >
+                          {item.rating}
+                        </Typography>
+                      </Box>
+                      <Typography
+                        variant="body2"
+                        color="primary"
+                        mt={1}
+                        fontWeight="bold"
+                      >
+                        {item.price}
+                      </Typography>
+                    </CardContent>
+                  </Card>
+                </Grid>
+              ))}
+            </Grid>
+          </Box>
+        ))}
+      </Box>
     </Stack>
   );
 }
