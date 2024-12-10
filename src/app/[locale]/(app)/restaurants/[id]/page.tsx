c2e700dd689cac85328cacc3df6fb3f89b6dee32
fakhribenfraj <benfrajfakhreddine15@gmail.com>
2024-12-10 12:51:31 +0100

    updated resto page

--- src/app/[locale]/(app)/restaurants/[id]/page.tsx
+++ src/app/[locale]/(app)/restaurants/[id]/page.tsx
@@ -1,20 +1,15 @@
 import { getRestaurantById } from "@/actions/restaurants/getRestaurantById";
-import Map from "@/components/common/surfaces/map/Map";
-import Marker from "@/components/common/surfaces/map/Marker";
 import Carousel from "@/components/custom/Carousel";
 import RestaurantNavTabs from "@/components/custom/restaurant/RestaurantNavTabs";
-import LocalPizzaIcon from "@mui/icons-material/LocalPizza";
-import {
-  Box,
-  Grid2,
-  ImageList,
-  ImageListItem,
-  Link,
-  Stack,
-  Typography,
-} from "@mui/material";
+import DeliveryDiningIcon from "@mui/icons-material/DeliveryDining";
+import EmailIcon from "@mui/icons-material/Email";
+import EventIcon from "@mui/icons-material/Event";
+import PhoneIcon from "@mui/icons-material/Phone";
+import RestaurantIcon from "@mui/icons-material/Restaurant";
+import RoomIcon from "@mui/icons-material/Room";
+import TakeoutDiningIcon from "@mui/icons-material/TakeoutDining";
+import { Box, Button, Grid, Grid2, Stack, Typography } from "@mui/material";
 import Image from "next/image";
-
 export default async function Page({
   params,
 }: {
@@ -70,13 +65,12 @@ export default async function Page({
       }&fit=crop&auto=format&dpr=2 2x`,
     };
   }
-  const rowHeight = 128;
   return (
     <Stack spacing={2} sx={{ pb: 4 }}>
       <RestaurantNavTabs id={id} active="overview" />
       <Grid2 container spacing={4}>
         <Grid2 size={{ xs: 12, md: 4 }}>
-          <Carousel>
+          <Carousel slidesToShow={3} centerMode>
             {itemData.map((item) => (
               <Box
                 key={item.title}
@@ -98,37 +92,102 @@ export default async function Page({
         </Grid2>
         <Grid2 size={{ xs: 12, md: 8 }}>
           <Stack spacing={2} sx={{ flex: 1 }}>
-            <Typography variant="body1">{restaurant.description}</Typography>
-            <Typography variant="body1">
-              visit website:
-              <Link href={`https://${restaurant.website}`} target="_blank">
-                {restaurant.website}
-              </Link>
-            </Typography>
             <Box
-              sx={{
-                borderRadius: 1,
-                border: "2px solid",
-                borderColor: "grey.300",
-                margin: "auto",
-                overflow: "hidden",
-              }}
+              display="flex"
+              justifyContent="space-between"
+              alignItems="center"
             >
-              <Map
-                initialViewState={{
-                  latitude: restaurant.lat,
-                  longitude: restaurant.lng,
-                  zoom: 7,
-                }}
-                style={{
-                  width: "20rem",
-                  height: "20rem",
-                }}
-              >
-                <Marker longitude={restaurant.lng} latitude={restaurant.lat}>
-                  <LocalPizzaIcon />
-                </Marker>
-              </Map>
+              <Typography variant="body2" color="text.secondary">
+                Hours
+              </Typography>
+              <Typography variant="body2" color="primary">
+                Open now | Closes at 9:30 PM
+              </Typography>
+            </Box>
+
+            {/* Description */}
+            <Box mt={2}>
+              <Typography variant="subtitle1" fontWeight="bold">
+                Description
+              </Typography>
+              <Typography variant="body2" color="text.secondary">
+                Award-winning Italian restaurant specializing in authentic pasta
+                and wood-fired pizzas
+              </Typography>
+            </Box>
+
+            {/* Services */}
+            <Box mt={2}>
+              <Typography variant="subtitle1" fontWeight="bold">
+                Services
+              </Typography>
+              <Grid2 container spacing={1} mt={1}>
+                <Grid2 size={6}>
+                  <Button
+                    variant="outlined"
+                    fullWidth
+                    startIcon={<DeliveryDiningIcon />}
+                    size="small"
+                  >
+                    Delivery
+                  </Button>
+                </Grid2>
+                <Grid2 size={6}>
+                  <Button
+                    variant="outlined"
+                    fullWidth
+                    startIcon={<TakeoutDiningIcon />}
+                    size="small"
+                  >
+                    Take away
+                  </Button>
+                </Grid2>
+                <Grid2 size={6}>
+                  <Button
+                    variant="outlined"
+                    fullWidth
+                    startIcon={<RestaurantIcon />}
+                    size="small"
+                  >
+                    Dine-In
+                  </Button>
+                </Grid2>
+                <Grid2 size={6}>
+                  <Button
+                    variant="outlined"
+                    fullWidth
+                    startIcon={<EventIcon />}
+                    size="small"
+                  >
+                    Private Events
+                  </Button>
+                </Grid2>
+              </Grid2>
+            </Box>
+
+            {/* Contact Info */}
+            <Box mt={2}>
+              <Typography variant="subtitle1" fontWeight="bold">
+                Restaurant Contact
+              </Typography>
+              <Box display="flex" alignItems="center" mt={1}>
+                <RoomIcon fontSize="small" color="action" />
+                <Typography ml={1} variant="body2" color="text.secondary">
+                  Tunis, Nabeul, Maaoura, 8013
+                </Typography>
+              </Box>
+              <Box display="flex" alignItems="center" mt={1}>
+                <EmailIcon fontSize="small" color="action" />
+                <Typography ml={1} variant="body2" color="text.secondary">
+                  Munch@gmail.com
+                </Typography>
+              </Box>
+              <Box display="flex" alignItems="center" mt={1}>
+                <PhoneIcon fontSize="small" color="action" />
+                <Typography ml={1} variant="body2" color="text.secondary">
+                  Mon-Fri (+216)12 122 122
+                </Typography>
+              </Box>
             </Box>
           </Stack>
         </Grid2>
