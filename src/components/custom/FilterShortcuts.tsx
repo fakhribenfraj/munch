1ffc69c4a6d0a66a9e42efe792449e2c59377fd7
fakhribenfraj <benfrajfakhreddine15@gmail.com>
2024-12-21 14:05:53 +0100

    made shortcuts as tabs

--- src/components/custom/FilterShortcuts.tsx
+++ src/components/custom/FilterShortcuts.tsx
@@ -12,7 +12,7 @@ const FilterShortcuts = ({ filters }: FilterShortcutsProps) => {
   const handleChange = (event: React.SyntheticEvent, newValue: number) => {
     setValue(newValue);
   };
-  const HEIGHT = 0;
+  const HEIGHT = 40;
   return (
     <Container maxWidth="md" disableGutters>
       <Tabs
@@ -20,12 +20,11 @@ const FilterShortcuts = ({ filters }: FilterShortcutsProps) => {
         scrollButtons="auto"
         value={value}
         onChange={handleChange}
-        TabIndicatorProps={{ hidden: true }}
+        indicatorColor="secondary"
         textColor="secondary"
         sx={{
           justifyContent: "center",
           minHeight: HEIGHT,
-          pb: 1,
         }}
       >
         {filters.map((filter) => (
@@ -34,19 +33,7 @@ const FilterShortcuts = ({ filters }: FilterShortcutsProps) => {
             label={filter}
             disableRipple
             sx={{
-              borderRadius: "16px",
-              px: 1,
-              py: 0.5,
               minHeight: HEIGHT,
-              border: "1px solid",
-              borderColor: "divider",
-              backgroundColor: "transparent",
-              transition: "background-color 0.5s,color 0.5s",
-
-              "&.Mui-selected": {
-                backgroundColor: "secondary.main",
-                color: "common.white",
-              },
             }}
           />
         ))}
